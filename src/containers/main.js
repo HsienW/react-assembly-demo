722aa4e68f5008e34299a83e5b99ba6a0dac7c54
hsien wei <hsien.w.wei@gmail.com>
2023-06-03 17:31:24 +0800

    update wasm import use case

--- src/containers/main.js
+++ src/containers/main.js
@@ -1,19 +1,31 @@
 import React, {useState, useEffect} from 'react';
-import wasmModule from '../../wasm/wasm-adder.wasm';
-import * as AsBind from 'as-bind';
+// import * as asBind from 'as-bind';
+// import wasmModule from '../../wasm/wasm-adder.wasm';
+
+// const wasmModule = fetch('../../wasm/wasm-adder.wasm');
 
 export const Main = () => {
     const [result, setResult] = useState(null);
 
     useEffect(() => {
         const fetchData = async () => {
-            const wasm = await AsBind.instantiate(wasmModule);
-            const {add} = wasm.exports;
-            const data = add(1, 2);
-            setResult(data);
+            // const wasm = await asBind.instantiate(wasmModule);
+            // const {add} = wasm.exports;
+            // const data = add(1, 2);
+            // setResult(data);
+
+            const response = await fetch('../../wasm/wasm-adder.wasm');
+            const buffer = await response.arrayBuffer();
+            const {instance} = await WebAssembly.instantiate(buffer);
+            return instance.exports.add(1, 2);
         };
 
-        fetchData();
+        fetchData()
+            .then((data) => {
+                console.log('success log');
+                console.log(data);
+                setResult(data);
+            });
     }, []);
 
     return (
